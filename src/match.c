255f2bbff8bc4cf0f43d2291cfb515b63832c599
GulgDev <programm.jeremiah@yandex.ru>
2025-06-01 14:05:01 +0500

    Improve matching for binary expressions

--- src/match.c
+++ src/match.c
@@ -1,33 +1,72 @@
 #include "match.h"
 #include "analyze.h"
+#include <stddef.h>
 
 static bool cmp_pattern(arvm_expr_t *expr, pattern_t *pattern) {
   switch (pattern->kind) {
   case EXPR_ANY:
     return true;
-  case EXPR_BINARY:
+  case EXPR_BINARY: {
     if (expr->kind != BINARY || expr->binary.op != pattern->binary.op)
       return false;
-    if (is_symmetric(pattern->binary.op))
-      return (matches(expr->binary.lhs, pattern->binary.lhs) ||
-              matches(expr->binary.lhs,
-                      BINARY(pattern->binary.op, pattern->binary.lhs,
-                             ANY(pattern->binary.rhs->capture))) ||
-              matches(expr->binary.rhs, pattern->binary.lhs) ||
-              matches(expr->binary.rhs,
-                      BINARY(pattern->binary.op, pattern->binary.lhs,
-                             ANY(pattern->binary.rhs->capture)))) &&
-             (matches(expr->binary.lhs, pattern->binary.rhs) ||
-              matches(expr->binary.lhs,
-                      BINARY(pattern->binary.op, pattern->binary.rhs,
-                             ANY(pattern->binary.rhs->capture))) ||
-              matches(expr->binary.rhs, pattern->binary.rhs) ||
-              matches(expr->binary.rhs,
-                      BINARY(pattern->binary.op, pattern->binary.rhs,
-                             ANY(pattern->binary.rhs->capture))));
-    else
+
+    arvm_expr_t *lhs, **lhs_src, *rhs, **rhs_src;
+
+    // If the binary operation is symmetric, we should check both sides
+    // recursively
+    if (is_symmetric(pattern->binary.op)) {
+      pattern_t lhs_pattern;
+      lhs_pattern = *pattern->binary.lhs;
+      lhs_pattern.capture = &lhs;
+
+      pattern_t rhs_pattern;
+      rhs_pattern = *pattern->binary.rhs;
+      rhs_pattern.capture = &rhs;
+
+      arvm_expr_t *subexpr;
+      if (matches(expr->binary.lhs,
+                  BINARY(pattern->binary.op, &lhs_pattern, ANY(), &subexpr))) {
+        lhs_src = &subexpr->binary.lhs;
+      } else if (matches(expr->binary.lhs, BINARY(pattern->binary.op, ANY(),
+                                                  &rhs_pattern, &subexpr))) {
+        rhs_src = &subexpr->binary.lhs;
+      }
+      if (matches(expr->binary.rhs,
+                  BINARY(pattern->binary.op, &lhs_pattern, ANY(), &subexpr))) {
+        lhs_src = &subexpr->binary.rhs;
+      } else if (matches(expr->binary.rhs, BINARY(pattern->binary.op, ANY(),
+                                                  &rhs_pattern, &subexpr))) {
+        rhs_src = &subexpr->binary.rhs;
+      }
+      if (!rhs || !lhs) {
+        lhs = expr->binary.lhs;
+        lhs_src = NULL;
+        rhs = expr->binary.rhs;
+        rhs_src = NULL;
+      }
+      if (matches(lhs, pattern->binary.rhs) &&
+          matches(rhs, pattern->binary.lhs)) {
+        arvm_expr_t *tmp = lhs, **tmp_src = lhs_src;
+        lhs = rhs;
+        lhs_src = rhs_src;
+        rhs = tmp;
+        rhs = tmp_src;
+      }
+      bool result = matches(lhs, pattern->binary.lhs) &&
+                    matches(rhs, pattern->binary.rhs);
+      if (result) {
+        if (lhs_src)
+          *lhs_src = expr->binary.lhs;
+        expr->binary.lhs = lhs;
+        if (rhs_src)
+          *rhs_src = expr->binary.lhs;
+        expr->binary.lhs = rhs;
+      }
+      return result;
+    } else
       return matches(expr->binary.lhs, pattern->binary.lhs) &&
              matches(expr->binary.rhs, pattern->binary.rhs);
+  }
   case EXPR_IN_INTERVAL:
     return expr->kind == IN_INTERVAL &&
            matches(expr->in_interval.value, pattern->in_interval.value);
@@ -43,6 +82,8 @@ static bool cmp_pattern(arvm_expr_t *expr, pattern_t *pattern) {
 }
 
 bool matches(arvm_expr_t *expr, pattern_t *pattern) {
+  if (expr == NULL)
+    return false;
   bool result = cmp_pattern(expr, pattern);
   if (result)
     *pattern->capture = expr;
