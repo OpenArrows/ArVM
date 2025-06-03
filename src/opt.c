9c54beec38b21c3b1422457a2d1e54036ff3ee3e
GulgDev <programm.jeremiah@yandex.ru>
2025-06-03 21:20:54 +0500

    Update optimizer

--- src/opt.c
+++ src/opt.c
@@ -7,6 +7,7 @@
 #include "visit.h"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 // TODO: is this needed
 static void substitute(arvm_expr_t *expr, arvm_expr_t *with,
@@ -17,6 +18,54 @@ static void substitute(arvm_expr_t *expr, arvm_expr_t *with,
 
 void arvm_optimize(arvm_expr_t *expr, void *ctx_) {
   arvm_opt_ctx_t *ctx = ctx_;
+
+  {
+    // Unwrap n-ary expressions
+    arvm_expr_t *child;
+    if (matches(expr, NARY_FIXED(ANYVAL(), ANY_AS(child)))) {
+      clone_expr(ctx->arena, child, expr);
+      return;
+    }
+  }
+
+  {
+    // Fold n-ary expressions
+    if (matches(expr, NARY(ANYVAL(), NARY(VAL(expr->nary.op))))) {
+      size_t argc = expr->nary.args.size;
+      arvm_expr_t **argv = expr->nary.args.exprs;
+
+      expr->nary.args.size = 0;
+      for (int i = 0; i < argc; i++) {
+        arvm_expr_t *arg = argv[i];
+        expr->nary.args.size +=
+            arg->kind == NARY && arg->nary.op == expr->nary.op
+                ? arg->nary.args.size
+                : 1;
+      }
+
+      expr->nary.args.exprs =
+          arena_alloc(ctx->arena, sizeof(arvm_expr_t *) * expr->nary.args.size);
+
+      size_t offset = 0;
+      for (int i = 0; i < argc; i++) {
+        arvm_expr_t *arg = argv[i];
+        if (arg->kind == NARY && arg->nary.op == expr->nary.op) {
+          memcpy(&expr->nary.args.exprs[offset], arg->nary.args.exprs,
+                 sizeof(arvm_expr_t *) * arg->nary.args.size);
+          offset += arg->nary.args.size;
+        } else {
+          expr->nary.args.exprs[offset] = arg;
+          offset++;
+        }
+      }
+    }
+  }
+
+  {
+    // TODO: Compile-time n-ary evaluation
+  }
+
+  
 
   /*switch (expr->kind) {
   case BINARY: {
--- test/opt.c
+++ test/opt.c
@@ -10,145 +10,35 @@ void setUp(void) {}
 
 void tearDown(void) { arena_free(&arena); }
 
-/*
-// g(C1, C2) => C3
-void test_opt_binary_const_const(void) {
+void test_unwrap_nary() {
+  arvm_func_t func = {make_nary(&arena, OR, 1, make_arg_ref(&arena))};
+  arvm_optimize_fn(&func, &arena);
+
+  arvm_expr_t *exp = make_arg_ref(&arena);
+
+  TEST_ASSERT(is_identical(func.value, exp));
+}
+
+void test_fold_nary() {
   arvm_func_t func = {
-      make_binary(&arena, ADD, make_const(&arena, 1), make_const(&arena, 2))};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_const(&arena, 3);
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-
-// x | true => true
-void test_opt_any_or_true(void) {
-  arvm_func_t func = {make_binary(&arena, OR, make_ref(&arena, ARG),
-                                  make_const(&arena, ARVM_TRUE))};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_const(&arena, ARVM_TRUE);
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-
-// x | false => x
-void test_opt_any_or_false(void) {
-  arvm_func_t func = {make_binary(&arena, OR, make_ref(&arena, ARG),
-                                  make_const(&arena, ARVM_FALSE))};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_ref(&arena, ARG);
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-
-// x & true => x
-void test_opt_any_and_true(void) {
-  arvm_func_t func = {make_binary(&arena, AND, make_ref(&arena, ARG),
-                                  make_const(&arena, ARVM_TRUE))};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_ref(&arena, ARG);
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-
-// x & false => false
-void test_opt_any_and_false(void) {
-  arvm_func_t func = {make_binary(&arena, AND, make_ref(&arena, ARG),
-                                  make_const(&arena, ARVM_FALSE))};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_const(&arena, ARVM_FALSE);
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-
-// x in X | x in Y => x in (X JOIN Y)
-void test_opt_interval_or_interval(void) {
-  arvm_func_t func = {make_binary(
-      &arena, OR, make_in_interval(&arena, make_ref(&arena, ARG), 0, 10),
-      make_in_interval(&arena, make_ref(&arena, ARG), 5, 20))};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_in_interval(&arena, make_ref(&arena, ARG), 0, 20);
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-
-// x in X & x in Y => x in (X INTERSECT Y)
-void test_opt_interval_and_interval(void) {
-  arvm_func_t func = {make_binary(
-      &arena, AND, make_in_interval(&arena, make_ref(&arena, ARG), 0, 10),
-      make_in_interval(&arena, make_ref(&arena, ARG), 5, 20))};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_in_interval(&arena, make_ref(&arena, ARG), 5, 10);
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-
-// x + C1 + C2 => x1 + (C1 + C2)
-void test_opt_any_plus_const_plus_const(void) {
-  arvm_func_t func = {make_binary(
-      &arena, ADD,
-      make_binary(&arena, ADD, make_ref(&arena, ARG), make_const(&arena, 1)),
-      make_const(&arena, 2))};
+      make_nary(&arena, ADD, 3,
+                make_nary(&arena, ADD, 2, make_none(&arena), make_none(&arena)),
+                make_const(&arena, 2),
+                make_nary(&arena, ADD, 3, make_none(&arena), make_none(&arena),
+                          make_none(&arena)))};
   arvm_optimize_fn(&func, &arena);
 
   arvm_expr_t *exp =
-      make_binary(&arena, ADD, make_ref(&arena, ARG), make_const(&arena, 3));
+      make_nary(&arena, ADD, 6, make_none(&arena), make_none(&arena),
+                make_const(&arena, 2), make_none(&arena), make_none(&arena),
+                make_none(&arena));
 
   TEST_ASSERT(is_identical(func.value, exp));
 }
-
-// x + C in [A, B] => x in [A - C, B - C]
-void test_opt_any_plus_const_in_interval(void) {
-  arvm_func_t func = {make_in_interval(
-      &arena,
-      make_binary(&arena, ADD, make_ref(&arena, ARG), make_const(&arena, -1)),
-      0, 10)};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_in_interval(&arena, make_ref(&arena, ARG), 1, 11);
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-
-void test_opt_inline_call(void) {
-  arvm_func_t f = {
-      make_binary(&arena, ADD, make_ref(&arena, ARG), make_const(&arena, -1))};
-
-  arvm_func_t func = {make_call(
-      &arena, &f,
-      make_binary(&arena, ADD, make_ref(&arena, ARG), make_const(&arena, -1)))};
-  arvm_optimize_fn(&func, &arena);
-
-  arvm_expr_t *exp = make_binary(
-      &arena, AND,
-      make_binary(&arena, ADD, make_ref(&arena, ARG), make_const(&arena, -2)),
-      make_in_interval(&arena, make_ref(&arena, ARG), 2,
-                       ARVM_POSITIVE_INFINITY));
-
-  TEST_ASSERT(is_identical(func.value, exp));
-}
-*/
 
 int main(void) {
   UNITY_BEGIN();
-  /*
-  RUN_TEST(test_opt_binary_const_const);
-  RUN_TEST(test_opt_any_or_true);
-  RUN_TEST(test_opt_any_or_false);
-  RUN_TEST(test_opt_any_and_true);
-  RUN_TEST(test_opt_any_and_false);
-  RUN_TEST(test_opt_interval_or_interval);
-  RUN_TEST(test_opt_interval_and_interval);
-  RUN_TEST(test_opt_any_plus_const_plus_const);
-  RUN_TEST(test_opt_any_plus_const_in_interval);
-  RUN_TEST(test_opt_inline_call);
-  */
+  RUN_TEST(test_unwrap_nary);
+  RUN_TEST(test_fold_nary);
   return UNITY_END();
 }
