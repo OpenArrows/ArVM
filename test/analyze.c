4597b1dc3a7e78210f53c973b379b784f3e54cd0
GulgDev <programm.jeremiah@yandex.ru>
2025-06-10 17:33:33 +0500

    Add missing analyze.c tests for `has_calls` & `intervals_overlap`

--- test/analyze.c
+++ test/analyze.c
@@ -20,8 +20,37 @@ void test_is_identical(void) {
   TEST_ASSERT_FALSE(is_identical(&expr2, &expr3));
 }
 
+void test_has_calls(void) {
+  arvm_expr_t const_ = {CONST, .const_ = {0}};
+  arvm_expr_t call = {CALL, .call = {NULL, &const_}};
+  arvm_expr_t expr1 = {NARY,
+                       .nary = {ADD, {2, (arvm_expr_t *[]){&const_, &call}}}};
+  arvm_expr_t expr2 = {NARY,
+                       .nary = {ADD, {2, (arvm_expr_t *[]){&const_, &const_}}}};
+  TEST_ASSERT(has_calls(&expr1));
+  TEST_ASSERT_FALSE(has_calls(&expr2));
+}
+
+void test_intervals_overlap(void) {
+  arvm_expr_t unk = {UNKNOWN};
+  TEST_ASSERT(intervals_overlap(
+      &(arvm_expr_t){IN_INTERVAL, .in_interval = {&unk, 0, 10}},
+      &(arvm_expr_t){IN_INTERVAL, .in_interval = {&unk, 5, 15}}));
+  TEST_ASSERT(intervals_overlap(
+      &(arvm_expr_t){IN_INTERVAL, .in_interval = {&unk, 0, 10}},
+      &(arvm_expr_t){IN_INTERVAL, .in_interval = {&unk, 10, 20}}));
+  TEST_ASSERT(intervals_overlap(
+      &(arvm_expr_t){IN_INTERVAL, .in_interval = {&unk, 0, 10}},
+      &(arvm_expr_t){IN_INTERVAL, .in_interval = {&unk, -10, 0}}));
+  TEST_ASSERT_FALSE(intervals_overlap(
+      &(arvm_expr_t){IN_INTERVAL, .in_interval = {&unk, 0, 10}},
+      &(arvm_expr_t){IN_INTERVAL, .in_interval = {&unk, 15, 20}}));
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_is_identical);
+  RUN_TEST(test_has_calls);
+  RUN_TEST(test_intervals_overlap);
   return UNITY_END();
 }
