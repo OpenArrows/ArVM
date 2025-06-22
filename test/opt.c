fdf7ede43fe630679453d6b992fcef568af14e6f
GulgDev <programm.jeremiah@yandex.ru>
2025-06-22 11:25:55 +0500

    Range optimizations

--- test/opt.c
+++ test/opt.c
@@ -18,16 +18,16 @@ void tearDown(void) { arvm_arena_free(&arena); }
     TEST_ASSERT(arvm_is_identical(func.value, expected));                      \
   } while (0)
 
-#define TEST_OPT(op) TEST_OPT_EQUAL(ORIGINAL(op), OPTIMIZED(op));
+#define TEST_NARY_OPT(op) TEST_OPT_EQUAL(ORIGINAL(op), OPTIMIZED(op));
 
-#define TEST_OPT_PRESERVED(op) TEST_OPT_EQUAL(ORIGINAL(op), ORIGINAL(op));
+#define TEST_NARY_OPT_PRESERVED(op) TEST_OPT_EQUAL(ORIGINAL(op), ORIGINAL(op));
 
 void test_unwrap_nary(void) {
 #define ORIGINAL(op) arvm_new_nary(&arena, op, 1, arvm_new_range(&arena, 0, 1))
 #define OPTIMIZED(op) arvm_new_range(&arena, 0, 1)
 
-  ARVM_OPS_ASSOCIATIVE(TEST_OPT)
-  ARVM_OPS_NON_ASSOCIATIVE(TEST_OPT_PRESERVED)
+  ARVM_OPS_ASSOCIATIVE(TEST_NARY_OPT);
+  ARVM_OPS_NON_ASSOCIATIVE(TEST_NARY_OPT_PRESERVED);
 
 #undef ORIGINAL
 #undef OPTIMIZED
@@ -36,81 +36,62 @@ void test_unwrap_nary(void) {
 void test_fold_nary(void) {
 #define ORIGINAL(op)                                                           \
   arvm_new_nary(&arena, op, 3,                                                 \
-                arvm_new_nary(&arena, op, 2, arvm_new_range(&arena, 0, 1),     \
-                              arvm_new_range(&arena, 1, 2)),                   \
+                arvm_new_nary(&arena, op, 2, arvm_new_modeq(&arena, 2, 1),     \
+                              arvm_new_modeq(&arena, 3, 2)),                   \
                 arvm_new_nary(&arena,                                          \
                               op == ARVM_OP_OR ? ARVM_OP_XOR : ARVM_OP_OR, 2,  \
-                              arvm_new_range(&arena, 2, 3),                    \
-                              arvm_new_range(&arena, 3, 4)),                   \
-                arvm_new_range(&arena, 4, 5))
+                              arvm_new_modeq(&arena, 5, 1),                    \
+                              arvm_new_modeq(&arena, 4, 2)),                   \
+                arvm_new_range(&arena, 0, 10))
 #define OPTIMIZED(op)                                                          \
   arvm_new_nary(                                                               \
-      &arena, op, 4, arvm_new_range(&arena, 0, 1),                             \
-      arvm_new_range(&arena, 1, 2),                                            \
+      &arena, op, 4, arvm_new_modeq(&arena, 2, 1),                             \
+      arvm_new_modeq(&arena, 3, 2),                                            \
       arvm_new_nary(                                                           \
           &arena,                                                              \
           op == ARVM_OP_OR ? ARVM_OP_XOR                                       \
                            : ARVM_OP_OR /* only operations of                  \
                                            the same kind should be folded */   \
           ,                                                                    \
-          2, arvm_new_range(&arena, 2, 3), arvm_new_range(&arena, 3, 4)),      \
-      arvm_new_range(&arena, 4, 5))
+          2, arvm_new_modeq(&arena, 5, 1), arvm_new_modeq(&arena, 4, 2)),      \
+      arvm_new_range(&arena, 0, 10))
 
-  ARVM_OPS_ASSOCIATIVE(TEST_OPT)
-  ARVM_OPS_NON_ASSOCIATIVE(TEST_OPT_PRESERVED)
+  ARVM_OPS_ASSOCIATIVE(TEST_NARY_OPT);
+  ARVM_OPS_NON_ASSOCIATIVE(TEST_NARY_OPT_PRESERVED);
 
 #undef ORIGINAL
 #undef OPTIMIZED
 }
 
-/*void test_normalize_interval(void) {
-  struct arvm_func func = {arvm_new_range(
-      &arena,
-      arvm_new_nary(&arena, ARVM_NARY_TH2, 2, arvm_new_expr(&arena, UNKNOWN),
-                    arvm_new_const(&arena, -2)),
-      0, ARVM_POSITIVE_INFINITY)};
-  arvm_optimize_func(&func, &arena, &arena);
+void test_union_ranges(void) {
+#define ORIGINAL(op)                                                           \
+  arvm_new_nary(&arena, op, 2, arvm_new_range(&arena, 0, 7),                   \
+                arvm_new_range(&arena, 3, 10))
+#define OPTIMIZED_ASSOCIATIVE(op) arvm_new_range(&arena, 0, 10)
+#define OPTIMIZED_NON_ASSOCIATIVE(op)                                          \
+  arvm_new_nary(&arena, op, 1, arvm_new_range(&arena, 0, 10))
 
-  arvm_expr_t exp = arvm_new_range(&arena, arvm_new_expr(&arena, UNKNOWN), 2,
-                                   ARVM_POSITIVE_INFINITY);
+#define OPTIMIZED OPTIMIZED_ASSOCIATIVE
+  TEST_NARY_OPT(ARVM_OP_OR);
+#undef OPTIMIZED
+#define OPTIMIZED OPTIMIZED_NON_ASSOCIATIVE
+  TEST_NARY_OPT(ARVM_OP_NOR);
+  TEST_NARY_OPT_PRESERVED(ARVM_OP_XOR);
+  TEST_NARY_OPT_PRESERVED(ARVM_OP_TH2);
 
-  TEST_ASSERT(arvm_is_identical(func.value, exp));
+#undef ORIGINAL
+#undef OPTIMIZED_ASSOCIATIVE
+#undef OPTIMIZED_NON_ASSOCIATIVE
+#undef OPTIMIZED
+
+  TEST_OPT_EQUAL(
+      arvm_new_nary(&arena, ARVM_OP_OR, 2, arvm_new_range(&arena, 0, 4),
+                    arvm_new_range(&arena, 5, 10)),
+      arvm_new_nary(&arena, ARVM_OP_OR, 2, arvm_new_range(&arena, 0, 4),
+                    arvm_new_range(&arena, 5, 10)));
 }
 
-void test_eval_interval(void) {
-  struct arvm_func func = {
-      arvm_new_range(&arena, arvm_new_const(&arena, 1), 0, 3)};
-  arvm_optimize_func(&func, &arena, &arena);
-
-  arvm_expr_t exp = arvm_new_const(&arena, ARVM_TRUE);
-
-  TEST_ASSERT(arvm_is_identical(func.value, exp));
-}
-
-void test_merge_intervals(void) {
-  struct arvm_func func = {arvm_new_nary(
-      &arena, ARVM_NARY_OR, 2,
-      arvm_new_range(&arena, arvm_new_expr(&arena, UNKNOWN), 0, 5),
-      arvm_new_range(&arena, arvm_new_expr(&arena, UNKNOWN), 3, 10))};
-  arvm_optimize_func(&func, &arena, &arena);
-
-  arvm_expr_t exp =
-      arvm_new_range(&arena, arvm_new_expr(&arena, UNKNOWN), 0, 10);
-
-  TEST_ASSERT(arvm_is_identical(func.value, exp));
-
-  func = (struct arvm_func){arvm_new_nary(
-      &arena, ARVM_NARY_AND, 2,
-      arvm_new_range(&arena, arvm_new_expr(&arena, UNKNOWN), 0, 5),
-      arvm_new_range(&arena, arvm_new_expr(&arena, UNKNOWN), 3, 10))};
-  arvm_optimize_func(&func, &arena, &arena);
-
-  exp = arvm_new_range(&arena, arvm_new_expr(&arena, UNKNOWN), 3, 5);
-
-  TEST_ASSERT(arvm_is_identical(func.value, exp));
-}
-
-void test_annulment_law(void) {
+/*void test_annulment_law(void) {
   struct arvm_func func = {arvm_new_nary(&arena, ARVM_NARY_AND, 2,
                                          arvm_new_expr(&arena, UNKNOWN),
                                          arvm_new_const(&arena, 0))};
@@ -231,16 +212,15 @@ void test_short_circuit(void) {
   arvm_expr_t exp = arvm_new_call(&arena, NULL, arvm_new_expr(&arena, UNKNOWN));
 
   TEST_ASSERT(arvm_is_identical(func.value->nary.operands.exprs[1], exp));
-}*/
+}
+*/
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_unwrap_nary);
   RUN_TEST(test_fold_nary);
-  /*RUN_TEST(test_normalize_interval);
-  RUN_TEST(test_eval_interval);
-  RUN_TEST(test_merge_intervals);
-  RUN_TEST(test_annulment_law);
+  RUN_TEST(test_union_ranges);
+  /*RUN_TEST(test_annulment_law);
   RUN_TEST(test_identity_law);
   RUN_TEST(test_idempotent_law);
   RUN_TEST(test_absorption_law);
